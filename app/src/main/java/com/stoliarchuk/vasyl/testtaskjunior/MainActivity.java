9322e5b89e5487549a0aaabda4ba1804010cae73
Vasyl Stoliarchuk <qloverock@gmail.com>
2017-08-01 10:45:40 +0300

    RssDownloadService downloads the RSS data

--- app/src/main/java/com/stoliarchuk/vasyl/testtaskjunior/MainActivity.java
+++ app/src/main/java/com/stoliarchuk/vasyl/testtaskjunior/MainActivity.java
@@ -1,13 +1,56 @@
 package com.stoliarchuk.vasyl.testtaskjunior;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tv, linkTv;
+    private BroadcastReceiver receiver = new Receiver();
+    private IntentFilter intentFilter = new IntentFilter(Receiver.INTENT_FILTER);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.tv);
+        linkTv = (TextView) findViewById(R.id.link_tv);
+        URL url = null;
+        try {
+            url = new URL(linkTv.getText().toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        if (null != url) {
+            RssDownloaderService.startDownloadRss(this, url);
+        }
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.unregisterReceiver(receiver);
+    }
+
+    class Receiver extends BroadcastReceiver{
+        public static final String INTENT_FILTER = "com.stoliarchuk.vasyl.testtaskjunior.INTENT_FILTER";
+
+        public void onReceive(Context context, Intent intent) {
+            String rss = intent.getStringExtra(Intent.EXTRA_TEXT);
+            tv.setText(rss);
+        }
+    }
+
 }
---
+++ app/src/main/java/com/stoliarchuk/vasyl/testtaskjunior/RssDownloaderService.java
@@ -0,0 +1,110 @@
+package com.stoliarchuk.vasyl.testtaskjunior;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class RssDownloaderService extends IntentService {
+
+    private final static String TAG = RssDownloaderService.class.getSimpleName();
+    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+    private static final String ACTION_DOWNLOAD_RSS = "com.stoliarchuk.vasyl.testtaskjunior.action.RSS";
+
+    private static final String EXTRA_LINK = "com.stoliarchuk.vasyl.testtaskjunior.extra.LINK";
+
+    public RssDownloaderService() {
+        super("RssDownloaderService");
+    }
+
+    /**
+     * Starts this service to perform action with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startDownloadRss(Context context, URL link) {
+        Intent intent = new Intent(context, RssDownloaderService.class);
+        intent.setAction(ACTION_DOWNLOAD_RSS);
+        intent.putExtra(EXTRA_LINK, link);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_DOWNLOAD_RSS.equals(action)) {
+                final URL link = (URL) intent.getSerializableExtra(EXTRA_LINK);
+                downloadRss(link);
+            }
+        }
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private void downloadRss(URL url) {
+
+        if (url == null) {
+            return;
+        }
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String responseJson = null;
+
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                return;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line + "\n");
+            }
+            if (buffer.length() == 0) {
+                Log.e(TAG, "Empty response");
+                return;
+            }
+            responseJson = buffer.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e(TAG, "Error closing stream", e);
+                }
+            }
+        }
+        sendBroadcast(new Intent(MainActivity.Receiver.INTENT_FILTER).putExtra(Intent.EXTRA_TEXT, responseJson));
+    }
+}
